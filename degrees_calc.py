6d6f1e1a48fc849c3be44f706d7c12b514efcd54
josh <jquahian@gmail.com>
2019-01-17 19:50:11 -0500

    added user input for gearing and counts

--- degrees_calc.py
+++ degrees_calc.py
@@ -1,9 +1,8 @@
-# 100,000 counts = ~35 - 40 degrees
 
 encoder_cpr = 8192
-gear_ratio = 64
-counts = 200000
+gear_ratio = int(input("Enter gearing reuduction: "))
+counts = int(input("Enter Encoder Counts: "))
 
 degrees = (counts / (gear_ratio * encoder_cpr)) * 360
 
-print(degrees)
+print("\nEXPECTED ROTATION: {} degrees".format(degrees))
