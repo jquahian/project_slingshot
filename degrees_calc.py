f9b69db64b9c5a7f1af9c2b6027f02047fbedee5
Joshua Qua Hiansen <jquahian@gmail.com>
2019-01-17 20:37:09 -0500

    added options menu and ability to return desired degree of movement or calculate degrees moved based on encoder counts

--- degrees_calc.py
+++ degrees_calc.py
@@ -1,8 +1,26 @@
-
 encoder_cpr = 8192
 gear_ratio = int(input("Enter gearing reuduction: "))
-counts = int(input("Enter Encoder Counts: "))
 
-degrees = (counts / (gear_ratio * encoder_cpr)) * 360
+def menu():
+	choice = input("\n(a) return counts \n(b) return degrees \n(c) quit \n\n")
 
-print("\nEXPECTED ROTATION: {} degrees".format(degrees))
+	if choice.lower() == "a":
+		return_counts()
+	elif choice.lower() =="b":
+		return_degrees()
+	else:
+		quit()
+
+def return_counts():
+	degrees = int(input("\nEnter degrees of rotation: "))
+	counts = (degrees / 360) * (gear_ratio * encoder_cpr)
+	print("\nENCODER COUNTS: {} to rotate {} degrees".format(counts, degrees))
+	menu()
+
+def return_degrees():
+	counts = int(input("\nEnter encoder counts: "))
+	degrees = (counts / (gear_ratio * encoder_cpr)) * 360
+	print("\nROTATION OF: {} degrees with {} encoder counts".format(degrees, counts))
+	menu()
+
+menu()
