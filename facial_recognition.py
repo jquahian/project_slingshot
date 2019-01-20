eb9e1d75984ecc3b9de7ad387a8258e1141350b3
Joshua Qua Hiansen <jquahian@gmail.com>
2019-01-19 19:44:03 -0500

    facial recognition module is working and calculating deltas

---
+++ facial_recognition.py
@@ -0,0 +1,77 @@
+import numpy as np
+import cv2
+import math
+
+# parts from: https://realpython.com/face-detection-in-python-using-a-webcam/
+
+font = cv2.FONT_HERSHEY_PLAIN
+font_size = 1
+main_font_color = (255, 255, 255)
+
+frontalface_path = 'C:/Users/Josh/Anaconda3/pkgs/opencv-3.3.1-py36h20b85fd_1/Library/etc/haarcascades/haarcascade_frontalface_default.xml'
+
+face_cascade = cv2.CascadeClassifier(frontalface_path)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # get width and length of the video stream
+    frame_width = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+    frame_length = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        center_point_x = int((2*x + w)/2)
+        center_point_y = int((2*y + h)/2)
+
+        # find the center point of the rectangle: remeber, it's BRG
+        cv2.circle(frame, (center_point_x, center_point_y), 5, (0, 0, 255), -1)
+
+        # display center coords for easy debugging later
+        cv2.putText(frame, (f'Center Point: ({center_point_x}, {center_point_y})'), (10, 25), font, font_size, main_font_color)
+
+    # draw a circle at the center point of the screen
+    cv2.circle(frame, (int(frame_width/2), int(frame_length/2)), 5, (255, 0, 0), -1)
+
+    # display text that shows the coords for the center of the screen
+    frame_mid_x = frame_width/2
+    frame_mid_y = frame_length/2
+    cv2.putText(frame, (f'Frame Center Point: ({frame_mid_x}, {frame_mid_y})'), (10, 50), font, font_size, main_font_color)
+
+    # display text that show the delta x and y between the center of the face and the center of the screen
+    delta_x = frame_mid_x - center_point_x
+    delta_y = frame_mid_y - center_point_y
+    cv2.putText(frame, (f'Deltas: ({delta_x}, {delta_y})'), (10, 75), font, font_size, main_font_color)
+
+    # assume 30 units from camera... fixe value until we get a range finder
+    ax_1_theta = math.degrees(np.arctan(delta_y/30))
+    cv2.putText(frame, (f'axis 1 degrees: ({ax_1_theta})'), (10, 100), font, font_size, main_font_color)
+
+    # pass the degrees calculated here back to odrive_control and re-position to space relative to current rotation on axis 1
+
+
+    # EVERYTHING GOES ABOVE THIS LINE #
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
--- odrive_control.py
+++ odrive_control.py
@@ -41,43 +41,44 @@ print("\nentering closed-loop control")
 drive_1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 drive_1.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
-# move off of home to center
-print("\nmoving to center")
+# move off of home to home position which is rougly centered
+print("\nmoving to home")
 drive_1.axis0.controller.pos_setpoint = ax0_centered
 drive_1.axis1.controller.pos_setpoint = ax1_centered
-print("axis 0 centered at: {} \naxis 1 centered at: {}".format(ax0_centered, ax1_centered))
+print("axis 0 homed at: {} \naxis 1 homed at: {}".format(ax0_centered, ax1_centered))
 time.sleep(3.0)
 
 # options to operate the arm
 def options():
 	print("\nselect command:")
-	sel = input("(a) position control \n(b) center \n(c) home \n(d) exit \nCOMMAND: ")
+	sel = input("(a) absolute rotation \n(b) home \n(c) exit \nCOMMAND: ")
 	if sel.lower() == 'a':
-		position()
+		absolute_rotation()
 	elif sel.lower() == 'b':
 		movement(ax0_centered, ax1_centered)
 	elif sel.lower() == 'c':
-		movement(ax0_min_lim, ax1_min_lim)
-	else:
 		exit_control()
+	else:
+		print("\ninvalid command")
+		options()
 
-# set the absolute position manually
-def position():
-	ax0_degs = int(input("\nEnter position for axis 0: "))
-	ax0_counts = int((ax0_degs / 360) * (ax0_gearing * encoder_cpr))
+# set the absolute rotation manually
+def absolute_rotation():
+	ax0_rot = int(input("\nEnter rotation (degrees) for axis 0: "))
+	ax0_counts = int((ax0_rot / 360) * (ax0_gearing * encoder_cpr))
 
 	if ax0_counts > ax0_max_lim or ax0_counts < ax0_min_lim:
 		print("axis 0 out of range!")
 		ax0_counts = 0
-		position()
+		absolute_rotation()
 
-	ax1_degs = int(input("Enter position for axis 1: "))
-	ax1_counts = int((ax1_degs / 360) * (ax1_gearing * encoder_cpr))
+	ax1_rot = int(input("Enter rotation (degrees) for axis 1: "))
+	ax1_counts = int((ax1_rot / 360) * (ax1_gearing * encoder_cpr))
 
 	if ax1_counts > ax1_max_lim or ax1_counts < ax0_min_lim:
 		print("axis 1 out of range!")
 		ax1_counts = 0
-		position()
+		absolute_rotation()
 
 	movement(ax0_counts, ax1_counts)
 
@@ -97,9 +98,9 @@ def movement(ax0_counts, ax1_counts):
 	options()
 
 def exit_control():
-	print("\nreturning home")
-	drive_1.axis0.controller.pos_setpoint = 0
-	drive_1.axis1.controller.pos_setpoint = 0
+	print("\nreturning to zero")
+	drive_1.axis0.controller.pos_setpoint = ax0_min_lim
+	drive_1.axis1.controller.pos_setpoint = ax0_min_lim
 	exit()
 
 options()
