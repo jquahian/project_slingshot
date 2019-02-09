cfc721181e68ef3dfac593e5056bbbac016a2d19
Joshua Qua Hiansen <jquahian@gmail.com>
2019-02-08 20:48:45 -0500

    removed menu from degrees calculator

--- degrees_calc.py
+++ degrees_calc.py
@@ -1,26 +1,11 @@
 encoder_cpr = 8192
-gear_ratio = int(input("Enter gearing reuduction: "))
 
-def menu():
-	choice = input("\n(a) return counts \n(b) return degrees \n(c) quit \n\n")
-
-	if choice.lower() == "a":
-		return_counts()
-	elif choice.lower() =="b":
-		return_degrees()
-	else:
-		quit()
-
-def return_counts():
+def return_counts(degrees, gear_ratio):
 	degrees = int(input("\nEnter degrees of rotation: "))
 	counts = (degrees / 360) * (gear_ratio * encoder_cpr)
-	print("\nENCODER COUNTS: {} to rotate {} degrees".format(counts, degrees))
-	menu()
+	return counts
 
-def return_degrees():
+def return_degrees(counts, gear_ratio):
 	counts = int(input("\nEnter encoder counts: "))
 	degrees = (counts / (gear_ratio * encoder_cpr)) * 360
-	print("\nROTATION OF: {} degrees with {} encoder counts".format(degrees, counts))
-	menu()
-
-menu()
+	return degrees
